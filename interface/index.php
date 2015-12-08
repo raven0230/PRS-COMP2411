f612c35ac05c2482e7d72023d770a20d562714d5
raven0230 <raven0230@gmail.com>
2015-12-09 05:55:20 +0800

    Add interface

---
+++ interface/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Conference Paper Review System</title>
+	<link rel="stylesheet" href="css/style.css">
+	</head>
+
+	<body>
+	<div id="wrap">
+	<div id="regbar">
+	<table width="1200px" class="tableCentre" >
+		<tr><td width="90%"><div id="leftmargin"><h2>Paper Review System</h2></div></td>
+		<td align="right">
+		<div id="rightmargin">
+		<h2><a href="#" id="loginform">Login</a></h2></td>
+		<tr><td><p>&nbsp </p></td><td>
+		<div class="login">
+		<div class="arrow-up"></div>
+		<div class="formholder">
+		<div class="randompad">
+		<fieldset>
+			<label name="email">Email</label>
+			<input type="email"/>
+			<label name="password">Password</label>
+			<input type="password" />
+			<input type="submit" value="Login" />
+
+		</fieldset>
+		</div>
+		</div>
+		</div>
+		</div>
+		</td></tr>
+		
+		</table>
+	</div>
+	</div>
+	<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+	<script src="js/index.js"></script>
+	<table width="1200px" class="tableCentre" >
+	<tr><td>
+	<div class="fontsforweb_fontid_423">
+	<ul>
+	  <li><a href="#"><font color="000000">Home</a></li>
+	  <li><a href="#"><font color="000000">Your work</a></li>
+	  <li><a href="#"><font color="000000">Search Paper</a></li>
+	</ul>
+	</div>
+	</td><td></td></tr></table>
+	</body>
+</html>
