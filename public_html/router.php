6763beb93ff49d094c03a8118d0607555af904df
hulloanson <hulloanson@gmail.com>
2015-12-10 16:20:36 +0800

    lalalalalalalala

--- public_html/router.php
+++ public_html/router.php
@@ -3,7 +3,8 @@ switch ($_POST["action"]) {
     case "login":
         $loginDB = new Login();
         if ($loginDB->checkCred($_POST["email"], $_POST["password"])) {
-
+            setcookie("userId", )
+            header("Location: index.php");
         }
         break;
     default:
